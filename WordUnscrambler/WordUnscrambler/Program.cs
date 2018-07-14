ac0e930089f6927f0cb575c2828fd433cbef57d2
whitefire0 <rickhallett@live.co.uk>
2018-07-14 13:58:39 +0100

    WordUnscrambler general structure 1

--- WordUnscrambler/WordUnscrambler/Program.cs
+++ WordUnscrambler/WordUnscrambler/Program.cs
@@ -6,7 +6,37 @@ namespace WordUnscrambler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool continueWordUnscramble = true;
+            do
+            {
+                Console.WriteLine("Please enter the option - M for manual, F for file");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("Enter scrambled words filename: ");
+                        ExecuteScrambledWordsInFile();
+                        break;
+                    case "M":
+                        Console.Write("Enter scrambled words manually:");
+                        ExecuteScrambledWordsManual();
+                        break;
+                    default:
+                        Console.WriteLine("Option was not recognised");
+                        break;
+                }
+            } while (continueWordUnscramble);
+        }
+
+        private static void ExecuteScrambledWordsManual()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ExecuteScrambledWordsInFile()
+        {
+            throw new NotImplementedException();
         }
     }
 }
