59c418e3de3d9ef52582aef1df1ce5e5483ff1c8
whitefire0 <rickhallett@live.co.uk>
2018-07-14 12:46:21 +0100

    CalculatorEngine Calculate

--- SimpleCalc/SimpleCalc/Program.cs
+++ SimpleCalc/SimpleCalc/Program.cs
@@ -6,19 +6,20 @@ namespace SimpleCalc
     {
         static void Main(string[] args)
         {
-            Input Converter inputConverter = new InputConverter();
+            InputConverter inputConverter = new InputConverter();
             CalculatorEngine calculatorEngine = new CalculatorEngine();
 
             double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
             double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
             string operation = Console.ReadLine();
 
-            double result = calculatorEngine.Calculate(operation, firstNumer, secondNumber);
+            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
             Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 
-    public class InputCoverter
+    public class InputConverter
     {
         public double ConvertInputToNumeric(string argTextInput)
         {
@@ -27,6 +28,36 @@ namespace SimpleCalc
             {
                 return convertedNumber;
             }
+        }
+    }
+
+    public class CalculatorEngine
+    {
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "subtract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                case "divide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognised");
+            }
+            return result;
         }
     }
 }
