43bc4db1b887309b38c60ef2fa0626b5eb83e04c
whitefire0 <rickhallett@live.co.uk>
2018-07-14 12:35:53 +0100

    InputConverter ConvertInputToNumeric

--- SimpleCalc/SimpleCalc/Program.cs
+++ SimpleCalc/SimpleCalc/Program.cs
@@ -22,7 +22,11 @@ namespace SimpleCalc
     {
         public double ConvertInputToNumeric(string argTextInput)
         {
-
+            double convertedNumber;
+            if(!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected a numeric value");
+            {
+                return convertedNumber;
+            }
         }
     }
 }
