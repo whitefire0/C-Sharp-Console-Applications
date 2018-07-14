8122b9871bddbe123d0dc749c22e3f9124b8be58
whitefire0 <rickhallett@live.co.uk>
2018-07-14 13:16:28 +0100

    SimpleCalc.Unit.Test CalculatorEngineTest

---
+++ SimpleCalc/SimpleCalc.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalc.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine _calclatorEngine = new CalculatorEngine();
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calclatorEngine.Calculate("add", number1, number2);
+        }
+    }
+}
---
+++ SimpleCalc/SimpleCalc.Unit.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalc.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+    {
+        private readonly CalculatorEngine _calclatorEngine = new CalculatorEngine();
+
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultForNonSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calclatorEngine.Calculate("add", number1, number2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
